f2c010a0b054268d11f5bbcbcefcdef0c27cafe1
carlos <bobcn_1991@hotmail.com>
2017-11-02 15:11:52 +0000

    pip install --upgrade google-api-python-client

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import pprint
 from flask import Flask, render_template
+from googleapiclient.discovery import build
+#custom search engine ID 000201262982528788274:btxholjtuq0
+#api key AIzaSyDjCVuiMp4eT0wLrRlGhmD7DsU_h8f7a-I
 
 app = Flask(__name__)
 @app.route('/')
