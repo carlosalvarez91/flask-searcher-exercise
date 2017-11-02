3ae75c90e59ddd1eff24d517309a942146f89354
carlos <bobcn_1991@hotmail.com>
2017-11-02 16:35:32 +0000

    test google custom search
    https://github.com/google/google-api-python-client/tree/master/samples/c
    ustomsearch

--- app.py
+++ app.py
@@ -4,10 +4,24 @@ from googleapiclient.discovery import build
 #custom search engine ID 000201262982528788274:btxholjtuq0
 #api key AIzaSyDjCVuiMp4eT0wLrRlGhmD7DsU_h8f7a-I
 
+def main():
+  # Build a service object for interacting with the API. Visit
+  # the Google APIs Console <http://code.google.com/apis/console>
+  # to get an API key for your own application.
+  service = build("customsearch", "v1",
+            developerKey="AIzaSyABO4RjWa3GQSu11Sa9Y6FKy48rth4v_rs")
+
+  res = service.cse().list(
+      q='lectures',
+      cx='000201262982528788274:btxholjtuq0',
+    ).execute()
+  pprint.pprint(res)
+
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
 
+main()
 if __name__ == '__main__':
     app.run(debug=True)
