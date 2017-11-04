badb632a9a26ae4cb400c11419a6953f915742ce
carlos <bobcn_1991@hotmail.com>
2017-11-04 01:11:28 +0000

    render links and searchtime at home template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import pprint
 import json
 import random
 from flask import Flask, render_template
 from googleapiclient.discovery import build
 #custom search engine ID 000201262982528788274:btxholjtuq0
 #api key AIzaSyDjCVuiMp4eT0wLrRlGhmD7DsU_h8f7a-I
+app = Flask(__name__)
 
 #https://github.com/google/google-api-python-client/blob/master/samples/customsearch/main.py
 def custom_search():
@@ -26,22 +26,21 @@ def custom_search():
     res_json = json.dumps(res,sort_keys=True, indent=4)#encoding res to json
     return (res['items'], res_json)
 
-#extract the 'link' value from res['items']
-res_links = custom_search()[0]
-for res_link in res_links:
-    print(res_link.get('link'))
-
-#extract the 'searchTime' value from res_json
-results = json.loads(custom_search()[1])#parse json
-searchTime = results['searchInformation']['searchTime'] # extract 'searchTime' value
-print(searchTime)
-
-
-app = Flask(__name__)
 @app.route('/')
-def index():
-    return render_template('home.html')
+def render_results():
+    #extract the 'link' value from res['items']
+    res_links = custom_search()[0]
+    #for res_link in res_links:  #this for loop at the front end
+        #links = res_link.get('link')
+        #print(links)
+    
+    #extract the 'searchTime' value from res_json
+    results = json.loads(custom_search()[1])#parse json
+    searchTime = results['searchInformation']['searchTime'] # extract 'searchTime' value
+    print(searchTime)
+    return render_template('home.html',res_links=res_links, searchTime=searchTime)
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
